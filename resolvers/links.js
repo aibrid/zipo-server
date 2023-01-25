722d02484fb62c2dc46419fa44e493b35a0fd923
adagiri <ibrahimridwan47@gmail.com>
2023-01-25 06:12:34 +0100

    ...

--- resolvers/links.js
+++ resolvers/links.js
@@ -3,17 +3,21 @@ const {
   isCustomizable,
   shortenLink,
   shortenCustomLink,
+  combineCustomLink,
   getOriginalLink,
+  getLinks,
 } = require('../controllers/links');
 const { protect, authorize } = require('../middleware/auth');
 
 module.exports = {
   Query: {
+    links: combineResolvers(protect, getLinks),
     link_isCustomizable: combineResolvers(isCustomizable),
     getOriginalLink: getOriginalLink,
   },
   Mutation: {
     link_shorten: combineResolvers(shortenLink),
     link_shortenCustom: combineResolvers(protect, shortenCustomLink),
+    link_combineCustom: combineResolvers(protect, combineCustomLink),
   },
 };
